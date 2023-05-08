3c2f477f555471635ea2ad627455dbe30aaf3c61
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2023-05-08 21:40:51 +0300

    Dummy push

--- FormManger.cpp
+++ FormManger.cpp
@@ -1,7 +1,7 @@
 #include "FormManger.h"
 
 
-void FormManger::Setscreen(sf::RenderWindow &win,tgui::GuiSFML &gui ,tgui::String User, tgui::String Usertype, tgui::String scrnNo="")
+void FormManger::Setscreen(sf::RenderWindow &win,tgui::GuiSFML &gui ,tgui::String User, tgui::String Usertype, tgui::String scrnNo)
 {
 	
 	gui.removeAllWidgets();
